06fc328707fae102177e6618a616226e5120dfc6
Екатерина Ножкина <65303200+EkaterinaNozhkina@users.noreply.github.com>
2020-05-14 14:28:04 +0500

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lib1.stpow("ha", 4));
+            Console.WriteLine(lib1.rev("hello"));
+            Console.WriteLine(lib1.remov("aHeallao", "a"));
+            Console.WriteLine(lib1.leng("Hello, friends!"));
+        }
+    }
+}
---
+++ lib1 (3).cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ViLibrary
+{
+    public class lib1
+    {
+        static public string stpow(string k, int b)
+        {
+            string k1 = k;
+            for (int s = 0; s < b - 1; s++)
+            {
+                k = k + k1;
+            }
+            return (k);
+        }
+        static public string rev(string a)
+        {
+            string l = "";
+            for (int s = a.Length - 1; s > -1; s--)
+            {
+                l = l + a[s] + "";
+            }
+            return (l);
+        }
+        static public int leng(string str)
+        {
+            int a = str.Length;
+            return a;
+        }
+
+        static public string remov(string str, string s)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(s, "");
+            }
+            return str;
+        }
+    }
+
+}
+
